2d0df3d6d3e1c3166e10d8b2c95553b33ec9e7ff
Victoriaadame <victoriaadame2468@gmail.com>
2025-06-22 16:40:15 -0700

    mouse

---
+++ script.js
@@ -0,0 +1,8 @@
+function draw() //This runs in a loop to update the display
+{
+if(mouseIsPressed) //if mouse is pressed
+   {
+
+   ellipse(mouseX, mouseY, 20, 20); //draws a circle at the mouse position
+   }
+}
