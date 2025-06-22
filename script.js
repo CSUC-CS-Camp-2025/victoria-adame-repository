be20da0fefcb2ce78bf590e78dcb0bec79cecb06
Victoriaadame <victoriaadame2468@gmail.com>
2025-06-22 16:55:34 -0700

    1

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
 window.onload = () => {
   document.querySelectorAll("p, img, h1").forEach(el => {
     el.style.opacity = 0;
-    el.style.transition = "opacity 10s";
+    el.style.transition = "opacity 1s";
     setTimeout(() => el.style.opacity = 1, 300);
   });
 };
